7aad54cecc1254961fd3e01ccc8520187395db3f
Your Name <you@example.com>
2021-07-10 01:09:14 +0200

    first commit

---
+++ src/main/java/com/timongisler/twwsd_Clone/twwsd_Clone/TwwsdCloneApplication.java
@@ -0,0 +1,13 @@
+package com.timongisler.twwsd_Clone.twwsd_Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwwsdCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwwsdCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timongisler/twwsd_Clone/twwsd_Clone/TwwsdCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timongisler.twwsd_Clone.twwsd_Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwwsdCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

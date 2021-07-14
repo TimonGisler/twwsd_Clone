8c2566bf8e1cccc5e52cbdad2b1d12c32c9e398f
Your Name <you@example.com>
2021-07-14 14:01:51 +0200

    ne sry

--- src/main/java/com/timongisler/twwsd_Clone/twwsd_Clone/TwwsdCloneApplication.java
+++ src/main/java/com/timongisler/twwsd_Clone/twwsd_Clone/TwwsdCloneApplication.java
@@ -2,12 +2,20 @@ package com.timongisler.twwsd_Clone.twwsd_Clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TwwsdCloneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwwsdCloneApplication.class, args);
+	}
+	
+	@GetMapping
+	public String Hello() {
+		return "Hello WOrld";
 	}
 
 }
